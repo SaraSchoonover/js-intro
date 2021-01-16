0f3d38990661ad1afdc488c7de45ea633db227d6
SaraSchoonover <saramschoon@gmail.com>
2021-01-16 11:58:27 -0600

    added functions with parameteters

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Connected");
+//console.log("Connected");
+
+//const sayHello = () => {
+  //console.log("Hello");
+//}
+
+//sayHello();
+
+
+// ******* FUNCTIONS WITH ARGUMENTS ******
+
+//function logsHello(name) {
+  ////console.log(`Hello, ${name}!`);
+//}
+
+//logsHello('Sara');
+
+const addTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log(sum);
+  return sum;
+}
+
+console.log(addTwoNumbers(5,7));
