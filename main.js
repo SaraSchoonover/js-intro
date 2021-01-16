93af92a04da86c883c33eec0667c7516b1485f58
SaraSchoonover <saramschoon@gmail.com>
2021-01-16 11:08:29 -0600

    added variables, data types and math to js files

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log("Connected");
+//console.log("CONNECTED");
+
+// ********* START VARIABLES *********
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+//console.log(firstName + lastName);
+//console.log(`First Name: ${firstName} ${lastName}`);
+
+firstName = 'Sara';
+lastName = 'Schoonover';
+
+//console.log(firstName + lastName);
+//console.log(`First Name: ${firstName} ${lastName}`);
+
+let favoriteFood = 'Tacos';
+//console.log(favoriteFood);
+
+ favoriteFood = "sushi";
+//console.log(favoriteFood);
+
+// ******** END VARIABLES ********
+
+// ********* START DATA TYPES *********
+
+//const dog = 'bark';
+
+//console.log(typeof dog);
+
+//const answer = 33;
+//const negative = -14;
+
+//console.log(typeof dog, typeof answer, typeof negative);
+
+//const iLoveJavascript = true;
+//console.log(typeof iLoveJavascript);
+
+
+// ********* END DATA TYPES *********
+
+// ********* START MATH *********
+
+console.log(Math.pow(3, 2));
+console.log(Math.pow(3, 3));
+
+console.log(Math.round(6.5));
+console.log(Math.round(6.45));
+console.log(Math.floor(6.9999));
+console.log(Math.ceil(6.0000001));
+
+const cat = "kitty";
+console.log(cat.length);
+
+
+
+// ********* END MATH *********
